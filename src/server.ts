ec20bf6d95accc7ebc0b2da832d813b2f524c5bd
Felipe Joazeiro <felipejoazeiro@gmail.com>
2021-04-22 12:31:12 -0300

    FrontEnd adicionado

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,40 @@
 import express from "express"
+import {createServer} from "http"
+import {Server, Socket} from "socket.io"
+import path from "path"
+
+
 import "./database"
 import {routes} from "./routes"
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, "..", "public")))
+app.set("views", path.join(__dirname,"..","public"))
+app.engine("html", require("ejs").renderFile)
+app.set("view engine", "html")
+
+app.get("/pages/client",(req,res)=>{
+    return res.render("html/client.html")
+
+})
+
+const http = createServer(app) // Criando protocolo http
+const io = new Server(http) //criando o protocolo de websocket
+
+io.on("connection",(socket: Socket)=>{
+    console.log("Se conectou", socket.id)
+})
+
 app.use(express.json())
 
 app.use(routes)
 
-app.listen(3333, ()=>{
-    console.log('Rodando na porta 3333')
+http.listen(3333, ()=>{
+    console.log('Server is running on port 3333')
 })
+
+
+/*app.listen(3333, ()=>{
+    console.log('Rodando na porta 3333')
+})*/
--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,4 +1,5 @@
-import { getCustomRepository } from "typeorm"
+import { getCustomRepository, Repository } from "typeorm"
+import { Setting } from "../entities/Setting"
 import { SettingRepository } from "../repositories/SettingsRepository"
 
 interface ISettingsCreate{
@@ -7,11 +8,16 @@ interface ISettingsCreate{
 }
 
 class SettingsService{
-    async create({chat, username}:ISettingsCreate){
-        const settingsRepository = getCustomRepository(SettingRepository)
+    private settingsRepository: Repository<Setting>
 
+    constructor(){
+        this.settingsRepository = getCustomRepository(SettingRepository)
+
+    }
+
+    async create({chat, username}:ISettingsCreate){
         
-        const userAlreadyExists = await settingsRepository.findOne({
+        const userAlreadyExists = await this.settingsRepository.findOne({
             username
         })
 
@@ -19,11 +25,11 @@ class SettingsService{
             throw new Error('User already exists!')
         }
 
-        const settings = settingsRepository.create({
+        const settings = this.settingsRepository.create({
             chat,
             username
         })
-        await settingsRepository.save(settings)
+        await this.settingsRepository.save(settings)
     }
 }
 
